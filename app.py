8f48519662b86b51d3d64d628bb1f37ed5745379
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2024-10-16 13:41:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1308,7 +1308,7 @@ def main():
     # Dicionário de arquivos de áudio, com nomes amigáveis mapeando para o caminho do arquivo
     mp3_files = {
         "Áudio explicativo para Leigos": "leigo.mp3",
-        "Áudio explicativo para treinamentos de poucos dados": "bucal.mp3",
+        "Áudio explicativo para treinamentos de poucos dados": "geografo.mp3",
     }
     
     # Lista de arquivos MP3 para seleção
